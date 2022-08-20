53b7c6a75f4555400b5c7a0f43e0c92686bee29a
Mark Alexander <markalexander@example.com>
2022-08-20 14:00:49 -0700

    Added basic express server alongside socketio server

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
 
-const io = require("socket.io");
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
-const server = new io.Server({
+const io = new Server(server, {
   cors: {
     origin: "http://localhost:8080"
   }
 });
 
-server.on("connection", (socket) => {
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on("connection", (socket) => {
   console.log("client connected");
 
   socket.on('test', (data) => {
     const d = JSON.parse(data);
     console.log(d.config.layers);
   });
+
   socket.on('disconnect', () => {
     console.log('client disconnected')
   })
