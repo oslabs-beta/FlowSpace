ac498959b55eb43b7f254065e853cce88624765a
MOakes7 <mike.oakes77@gmail.com>
2022-09-11 21:31:02 -0500

    replay and save functionality moved from mike/refactor

--- server.js
+++ server.js
@@ -1,90 +1,97 @@
 #!/usr/bin/env node
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 
 const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:8080", "http://localhost:8081"]
-  }
+	cors: {
+		origin: ["http://localhost:8080", "http://localhost:8081"],
+	},
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/build/index.html');
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/build/index.html");
 });
 
-app.get('/bundle.js', (req, res) => {
-  res.sendFile(__dirname + '/build/bundle.js');
+app.get("/bundle.js", (req, res) => {
+	res.sendFile(__dirname + "/build/bundle.js");
 });
 
 // model must be doubly parsed prior to using this function
 const parseModel = (model) => {
-  model = JSON.parse(model);
- 
-  let layer = {};
- 
-  for (let i = 0; i < model.config.layers.length; i++) {
- 
-    // check if it's the first layer or not to set input shape 
-    model.config.layers[i].config.batch_input_shape ?
-    input_shape = model.config.layers[i].config.batch_input_shape[1]
-    : input_shape = model.config.layers[i-1].config.units;
+	model = JSON.parse(model);
 
-    // set output shape
-    output_shape = model.config.layers[i].config.units;
+	let layer = {};
 
-    // calculate layer height 
-    const layer_height_px = 80 + ((output_shape - 1) * 100)
+	for (let i = 0; i < model.config.layers.length; i++) {
+		// check if it's the first layer or not to set input shape
+		model.config.layers[i].config.batch_input_shape
+			? (input_shape = model.config.layers[i].config.batch_input_shape[1])
+			: (input_shape = model.config.layers[i - 1].config.units);
 
-    // calculate connections
-    params = (input_shape * output_shape) + output_shape;
+		// set output shape
+		output_shape = model.config.layers[i].config.units;
 
+		// calculate layer height
+		const layer_height_px = 80 + (output_shape - 1) * 100;
 
-    layer[i] = 
-    {
-      layer_number: i,
-      layer_type: model.config.layers[i].config.name.includes('dense') ? 'DENSE' : 'NOT DENSE',
-      input_shape,
-      output_shape,
-      layer_height_px,
-      params
-    }
-  }
-  return layer;
-}
+		// calculate connections
+		params = input_shape * output_shape + output_shape;
 
-const lossData = [];
+		layer[i] = {
+			layer_number: i,
+			layer_type: model.config.layers[i].config.name.includes("dense")
+				? "DENSE"
+				: "NOT DENSE",
+			input_shape,
+			output_shape,
+			layer_height_px,
+			params,
+		};
+	}
+	return layer;
+};
+
+let savedData = [];
 
 io.on("connection", (socket) => {
-  console.log("client connected");
+	console.log("client connected");
 
-  socket.on('modelData', (data) => {
-    const d = parseModel(data);
-    io.sockets.emit('incomingData', d);
-  });
+	socket.on("modelData", (data) => {
+		const d = parseModel(data);
+		io.sockets.emit("incomingData", d);
+	});
 
-  socket.on('lossData', (data) => {
-    // console.log(data);
-    io.sockets.emit('sentLossData', data);
-    // lossData.push(data);
-    // console.log('lossData called, loss data: ', lossData);
-  });
+	socket.on("lossData", (data) => {
+		// console.log(data);
+		io.sockets.emit("sentLossData", data);
+		savedData.push(data);
+		console.log(`savedData size is ${savedData.length}`);
+	});
 
-  // socket.on('graphTab', () => {
-  //   console.log('graphTab called, loss data: ', lossData);
-  //   lossData.forEach((el) => {
-  //     io.sockets.emit('sentLossData', el);
-  //   });
-  // });
+	socket.on("check saved data", () => {
+		console.log("save button clicked ->", savedData);
+	});
 
-  socket.on('disconnect', () => {
-    console.log('client disconnected');
-  })
+	socket.on("replay", () => {
+		//console.log("Btn Clicked here's lossData ->", savedData);
+		savedData.forEach((el, index) => {
+			setTimeout(() => {
+				io.sockets.emit("sentLossData", el);
+				console.log("now sending ->", el);
+			}),
+				2.0 * 1000;
+		});
+	});
+
+	socket.on("disconnect", () => {
+		console.log("client disconnected");
+	});
 });
 
 server.listen(3333, () => {
-  console.log('Server listening on 3333');
+	console.log("Server listening on 3333");
 });
